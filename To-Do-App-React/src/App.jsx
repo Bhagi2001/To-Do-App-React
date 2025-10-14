054fa44da2a718ee4330b180c4acff75fca1d320
Bhagya Welivita <bhagyawelivita2021@gmail.com>
2025-10-15 01:10:38 +0530

    feat: implement Todo component and update App layout

--- To-Do-App-React/src/App.jsx
+++ To-Do-App-React/src/App.jsx
@@ -1,9 +1,15 @@
+import Todo from "./components/Todo"
 
 function App() {
 
   return (
     <>
     
+    <div className="grid min-h-screen py-4 bg-gradient-to-bl from-[#0f172a] via-[#1e1a78] to-[#0f172a]">
+
+
+    <Todo/>
+    </div>
     </>
   )
 }
